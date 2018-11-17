7448af4a33a04979bf99f576f982e8bae2ede092
himanshu arora <himanshu.arora-ext@kering.com>
2018-11-17 20:37:18 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "akka-training"
+
+version := "1.0.0-SNAPSHOT"
+
+scalaVersion := "2.12.7"
+        
---
+++ src/main/scala/com/neolynk/akka/Main.scala
@@ -0,0 +1,8 @@
+package com.neolynk.akka
+
+/**
+  * Created by himanshu on 17/11/2018.
+  */
+object Main {
+
+}
